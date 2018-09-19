c7724597f624371f8ccbf869ea7f40506c55b014
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-09-19 20:06:02 +0000

    update

--- index.js
+++ index.js
@@ -1,103 +1,75 @@
-const config = require('config')
-const { Pool, Query } = require('pg')
 const tilebelt = require('@mapbox/tilebelt')
+const { Pool, Query } = require('pg')
 const wkx = require('wkx')
-const fs = require('fs')
-const { spawnSync } = require('child_process')
 
-let modify
-if (fs.existsSync('./modify.js')) {
-  modify = require('./modify.js')
-} else {
-  modify = f => {return f}
+if (process.argv.length !== 6) {
+  console.log('usage: node index.js {config.js} {z} {x} {y}')
+  process.exit()
+}
+const config = require(process.argv[2])
+const Z = Number(process.argv[3])
+const X = Number(process.argv[4])
+const Y = Number(process.argv[5])
+const BBOX = tilebelt.tileToBBOX([X, Y, Z])
+let layerCount = config.data.length
+
+const featureDump = (row, geom, tippecanoe, modify) => {
+  let f = {
+    type: 'Feature',
+    geometry: wkx.Geometry.parse(Buffer.from(row[geom], 'hex')).toGeoJSON(),
+    tippecanoe: tippecanoe
+  }
+  delete row[geom]
+  f.properties = row
+  if (modify) { f = modify(f) }
+  console.log(JSON.stringify(f))
 }
 
-const data = config.get('data')
-let pools = {}
-for (let database of Object.keys(data)) {
-  pools[database] = new Pool({
-    host: config.get('host'),
-    user: config.get('user'),
-    password: config.get('password'),
+const dump = async (database, relation, geom, props, tippecanoe, modify) => {
+  let pool = new Pool({
+    host: config.host,
+    user: config.user,
+    password: config.password,
     database: database,
     max: 2
   })
-  pools[database].on('error', (err, client) => {
-    console.error(`unexpected error on idle client ${err} ${database} ${client}`)
+  pool.on('error', (err, client) => {
+    console.error(`unexpected error on ${database}`)
   })
+  const client = await pool.connect()
+  await client.query(new Query(`\
+WITH envelope AS 
+  (SELECT ST_MakeEnvelope(${BBOX.join(', ')}, 4326) as geom) 
+SELECT ${props.join(', ')}${props.length === 0 ? '' : ', '} 
+  ST_Intersection(ST_MakeValid(${relation}.${geom}), envelope.geom) as geom 
+FROM ${relation} JOIN envelope ON ${relation}.${geom} && envelope.geom
+`))
+    .on('row', row => {
+      featureDump(row, geom, tippecanoe, modify)
+    })
+    .on('error', err => {
+      console.log(err.stack)
+      client.release()
+    })
+    .on('end', () => {
+      layerCount--
+      client.release()
+      if (layerCount === 0) {
+        console.log('end')
+        process.exit()
+      }
+    })
 }
 
-const pnd = async function (module) {
-  const startTime = new Date()
-  if (fs.existsSync(`${module.join('-')}.ndjson`)) return
-  const stream = fs.createWriteStream(`${module.join('-')}.ndjson-part`)
-  const bbox = tilebelt.tileToBBOX([module[1], module[2], module[0]])
-  let layerCount = 0
-  for (const database of Object.keys(data)) {
-    layerCount += data[database].length
-    for (const layer of data[database]) {
-      const client = await pools[database].connect()
-      const geom = config.get('geom')[database]
-      let q = `WITH envelope AS (` +
-        `  SELECT ST_MakeEnvelope(${bbox.join(', ')}, 4326) as geom` +
-        `)` + 
-        `SELECT *, ` +
-        `  ST_Intersection(ST_MakeValid(${layer}.geom), envelope.geom) as geom ` +
-        `FROM ${layer} ` +
-        `JOIN envelope ` +
-        `ON ${layer}.${geom} && envelope.geom `
-      await client.query(new Query(q))
-        .on('row', row => {
-          let g = null
-          try {
-            g = wkx.Geometry.parse(Buffer.from(row[geom], 'hex')).toGeoJSON()
-          } catch (e) {
-            if (e instanceof RangeError) {
-              console.error(module.join('-') + ' buffer range error: ' + 
-                e.stack)
-              return
-            }
-            throw e
-          }
-          delete row[geom]
-          let properties = row
-          properties._layer = layer
-          let f = {
-            type: 'Feature',
-            geometry: g,
-            properties: properties
-          }
-          stream.write(JSON.stringify(modify(f)) + '\n')
-        })
-        .on('error', err => {
-          console.error(`${layer}/${module.join('-')} query: ${err.stack}`)
-          client.release()
-        })
-        .on('end', () => {
-          layerCount -= 1
-          client.release()
-          if (layerCount === 0) stream.end()
-        })
-    }
-  }
-  stream.on('close', () => {
-    fs.renameSync(
-      `${module.join('-')}.ndjson-part`, 
-      `${module.join('-')}.ndjson`
-    )
-    console.log(
-      `${module.join('-')} took ` + 
-      `${((new Date()).getTime() - startTime.getTime()) / 1000}s.`
-    )
-  })
-}
-
-async function main () {
-  for (const module of config.get('modules')) {
-    console.log(`importing ${module.join('-')}`)
-    if (!fs.existsSync(`${module.join('-')}.ndjson`)) {
-      await pnd(module)
-    }
+const main = async () => {
+  for (let datum of config.data) {
+    const [database, relation] = datum[0].split('::')
+    const geom = datum[1]
+    const props = datum[2]
+    const tippecanoe = datum[3]
+    const modify = datum[4]
+    if (Z < 6 && tippecanoe.minzoom >= 6) continue // safeguard
+    dump(database, relation, geom, props, tippecanoe, modify)
   }
 }
 
